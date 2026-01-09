5b4252acddcad99add1d9cb323db77e3f4806727
teuaraujoo <empresarial.mateus25@gmail.com>
2026-01-08 23:14:14 -0300

    exercicios

--- 1Projetos/cartaocredito.js
+++ 1Projetos/cartaocredito.js
@@ -1,7 +1,7 @@
-// CVV - 3 (VISA- MASTERCARD) OU 4 (AMEX) DÍGITOS
-// NUMERO DO CARTAO - 13 A 19 DIGÍTOS
-// BANDEIRA
-// VALIDADE - DATA PASSADA, MES 1 - 12
+// CVV - 3 (VISA- MASTERCARD) OU 4 (AMEX) DÍGITOS ⏳
+// NUMERO DO CARTAO - 13 A 19 DIGÍTOS ✅
+// BANDEIRA ✅
+// VALIDADE - DATA PASSADA, MES 1 - 12 ✅
 
 // opcional: bandeira (começo do num do cartao)
 /*
@@ -13,3 +13,54 @@ Discover - 6011
 */
 
 // Algoritmo de Luhn
+
+const tamanho = (num) => {
+    if (num.length >= 13 && num.length <= 19){
+        return true
+    } else {
+        return false
+    };
+};
+
+function bandeira(num){
+    if (num[0] === '4'){
+        return 'visa';
+    } else if (num.slice(0, 2) === '51'){
+        return 'mastercard';
+    } else if (num.slice(0, 2) === '34' || num.slice(0, 2) === '37'){
+        return 'amex';
+    } 
+    return 'nao identificado';
+}
+
+function cvv(n){
+    if (bandeira() === 'visa' || bandeira() === 'mastercard' && n.length === 3){
+        return true;
+    };
+    if (bandeira() === 'amex' && n.length === 4){
+        return true;
+    };
+    return false;
+}
+
+function verificarValidade(date){
+
+    let partes = date.split("/");
+    let mes = Number(partes[0]);
+    let ano = Number(partes[1]);
+
+    if (mes < 1 || mes > 12){
+        return 'mes invalido';
+    };
+
+    ano += 2000;
+
+    let validade = new Date(ano, mes);
+    let hoje = new Date();
+
+    if (hoje > validade){
+        return 'cartão inválido';
+    } else {
+        return 'cartao válido';
+    };
+};

f28974ad65717ad28709addbebbfafb6ea6c6002
teuaraujoo <empresarial.mateus25@gmail.com>
2026-01-17 15:54:58 -0300

    metodo

---
+++ métodos/split.js
@@ -0,0 +1,9 @@
+let frase = 'Olá, bom dia!';
+
+let spr1 = frase.split('');
+let spr2 = frase.split(' ');
+
+console.log('Separando sem espaço:');
+console.log(spr1);
+console.log('Separando com espaço:');
+console.log(spr2);

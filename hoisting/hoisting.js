1911db27ce77768474429e8b3ebd8c3e4440d834
teuaraujoo <empresarial.mateus25@gmail.com>
2026-01-17 23:11:05 -0300

    metodos, hoisting, escopo

---
+++ hoisting/hoisting.js
@@ -0,0 +1,14 @@
+saudar(); // Funciona!
+function saudar() {
+    console.log("Olá!");
+}
+
+console.log(nome); // Saída: undefined
+var nome = "Maria";
+console.log(nome); // Saída: Maria
+
+const x = 1;
+{
+    console.log(x); // ReferenceError: Cannot access 'x' before initialization
+    const x = 2;
+}

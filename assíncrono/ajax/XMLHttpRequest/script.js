70289488eee8ae6875e4a1056765e3e475895d82
teuaraujoo <empresarial.mateus25@gmail.com>
2026-02-22 01:01:20 -0300

    exercicios

--- assíncrono/ajax/XMLHttpRequest/script.js
+++ assíncrono/ajax/XMLHttpRequest/script.js
@@ -1,9 +1,44 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send(null);
-
-    xhr.addEventListener('load', () => {
-        
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send(null);
+    
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            };
+        });
     })
+};
+
+document.addEventListener("click", (e) => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === "a") {
+        e.preventDefault();
+        carregaPagina(el);
+    };
+});
+
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    try {
+        const response = await request({
+            method: 'GET',
+            url: href
+        });
+        carregaResultado(response);
+    } catch(e) {
+        console.log(e);
+    }
+};
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response; 
 }
